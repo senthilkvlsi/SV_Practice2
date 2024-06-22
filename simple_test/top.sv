fbe8419e8fc5517c851f02b86632f84e339be4b0
senthilkvlsi <67746170+senthilkvlsi@users.noreply.github.com>
2024-06-22 19:05:41 +0530

    Update top.sv

--- simple_test/top.sv
+++ simple_test/top.sv
@@ -15,6 +15,7 @@ module top;
   mem_new  #(AWIDTH,DWIDTH) mem  (clk,wrt,addr,data);
   
   // Drive the inout port conditionally
+  // Otherwise you cannot drive data continuosly in initial block
   assign data = wrt ? data_out : 'bz;
   
   initial begin
@@ -23,34 +24,40 @@ module top;
     $display("This is printed after 1 nanosecond delay");
     #1
     $display("Time=%f", $time);
-    
-    #1
+    #10
     addr = 10;
     wrt = 1;
     data_out = 10;
     
-    #1
+    #10
     addr = 20;
     wrt = 1;
+    //use of '1 here
+    data_out = '1;
     
-    #1
+    #10
     addr = 30;
     wrt = 1;
-    
-    #1
+    //use of '1 here
+    data_out = '0;
+        
+    #10
     wrt = 0;
+    addr = 40;
+    $display("addr=%h, data=%h", addr,data);
+    
+    #10
+    addr = 30;
+    $display("addr=%h, data=%h", addr,data);
+    
+    #10
+    addr = 20;
+    $display("addr=%h, data=%h", addr,data);
+    
+    #10
     addr = 10;
     $display("addr=%h, data=%h", addr,data);
-    
-    #1
-    addr = 30;
-    $display("addr=%h, data=%h", addr,data);
-    
-    #1
-    addr = 20;
-    $display("addr=%h, data=%h", addr,data);
-    
-    
+        
   end  
   
   
